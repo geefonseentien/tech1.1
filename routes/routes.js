2304cf9f2249bd45a4ef7620273e51a60ca9a0d7
lottekoblens <lotte.koblens@hva.nl>
2021-03-17 10:21:51 +0100

    teksten van de formulieren wat verduidelijkt

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const session = require('express-session')
 const router = express.Router()
 const bodyParser = require('body-parser')
 const { MongoClient } = require('mongodb')
